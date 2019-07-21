d24ad41149714fd465e54179e4e096cdd8ad93f6
Denis <shagalindenis@gmail.com>
2019-07-21 19:37:54 +0300

    savesds

---
+++ precache-manifest.b082934731a61fd4ad14ac85e77cfe73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b51ee6036d4eb0ecab2c",
+    "url": "/static/js/main.b51ee603.chunk.js"
+  },
+  {
+    "revision": "2f424459113649b97111",
+    "url": "/static/js/1.2f424459.chunk.js"
+  },
+  {
+    "revision": "b51ee6036d4eb0ecab2c",
+    "url": "/static/css/main.38219428.chunk.css"
+  },
+  {
+    "revision": "2f424459113649b97111",
+    "url": "/static/css/1.e8474e92.chunk.css"
+  },
+  {
+    "revision": "4701532a295478712c58b14a36b7f7bb",
+    "url": "/index.html"
+  }
+];
